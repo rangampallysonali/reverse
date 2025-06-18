fe18dc5f5a1db0107f28c0db1af4b28040beb692
Sonali <158792083+rangampallysonali@users.noreply.github.com>
2025-06-18 12:02:50 -0700

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,33 +1,25 @@
 public class Example
 {
-   public void reverseWordInMyString(String str)
+   public String reverseWordInMyString(String str)
    {
-	/* The split() method of String class splits
-	 * a string in several strings based on the
-	 * delimiter passed as an argument to it
-	 */
-	String[] words = str.split(" ");
-	String reversedString = "";
-	for (int i = 0; i < words.length; i++)
-        {
+       String[] words = str.split(" ");
+       String reversedString = "";
+       for (int i = 0; i < words.length; i++)
+       {
            String word = words[i]; 
            String reverseWord = "";
            for (int j = word.length()-1; j >= 0; j--) 
-	   {
-		/* The charAt() function returns the character
-		 * at the given position in a string
-		 */
-		reverseWord = reverseWord + word.charAt(j);
-	   }
-	   reversedString = reversedString + reverseWord + " ";
-	}
-	System.out.println(str);
-	System.out.println(reversedString);
+           {
+               reverseWord = reverseWord + word.charAt(j);
+           }
+           reversedString = reversedString + reverseWord + " ";
+       }
+       return reversedString;  // return instead of print
    }
    public static void main(String[] args) 
    {
-	Example obj = new Example();
-	obj.reverseWordInMyString("Welcome to BeginnersBook");
-	obj.reverseWordInMyString("This is an easy Java Program");
+       Example obj = new Example();
+       System.out.println(obj.reverseWordInMyString("Welcome to BeginnersBook"));
+       System.out.println(obj.reverseWordInMyString("This is an easy Java Program"));
    }
 }
